354b505721a013eea0a7becde428da181b32c142
kalpha <162287131+gguatit@users.noreply.github.com>
2025-11-18 05:06:49 +0000

    Fix: Configure for Cloudflare Workers with Assets

--- public/_worker.js
+++
@@ -1,7 +0,0 @@
-// Cloudflare Pages Functions Worker
-// This file is automatically deployed with your Pages project
-
-export { default } from '../src/worker.js';
-
-// Re-export Durable Object classes
-export { ChatRoom } from '../src/worker.js';
